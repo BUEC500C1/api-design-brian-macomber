ac041cf19d177cb965ee1335255a6dd35f4aac4f
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-02-07 17:32:02 -0500

    First push of airport files

--- app.py
+++ app.py
@@ -4,15 +4,36 @@ References:
 https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
 '''
 
-
+import csv
 import flask
 from flask import jsonify
+import requests
 
 
+def getGeoCoords(ident):
+    with open('airports.csv', mode='r') as airports_csv:
+        csv_reader = csv.reader(airports_csv, delimiter=',')
+        for row in csv_reader:
+            if ident == row[1]:
+                return (row[4], row[5])
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
+def getAirportData(lat, lon):
+    params = {"lat": lat, "lon": lon}
+    jsonData = requests.get("http://api.openweathermap.org/data/2.5/weather}", params=params)
+    print(jsonData.status_code)
+
+# app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+
+
+ident_name = "00AA"
+
+lat, lon = getGeoCoords(ident_name)
+getAirportData(lat, lon)
+
+
+'''
 # Create some test data for our catalog in the form of a list of dictionaries.
 books = [
     {'id': 0,
@@ -45,3 +66,4 @@ def api_all():
 
 
 app.run()
+'''
