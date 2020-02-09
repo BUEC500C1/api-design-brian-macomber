a2c17ec2eb2faed34b3450624c74631b943606d4
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-02-08 19:37:54 -0500

    successful data output to localhost, no testing yet

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # write tests for each of the sub functions:
 #   potentially put the helper functions in a different file
 # write a function to get the city name or airport name
+# maybe write in different returns for different status codes
 
 '''
 Brian Macomber - U25993688
@@ -37,49 +38,66 @@ def getGeoCoords(ident):
 
 # using the latitude and longitude, the current weather of that airport is returned
 def getAirportWeather(lat, lon):
-    params = {"lat": lat, "lon": lon, "appid": api_key}
-    url = "http://api.openweathermap.org/data/2.5/weather"
-    Data = requests.get(url, params=params)
-
-    if (Data.status_code == 200):
-        usefulData = Data.__dict__
-        weatherDataBytes = usefulData.get('_content')
-        weatherDataDict = json.loads(weatherDataBytes.decode('utf-8'))
-        return weatherDataDict
-    else:
+    if (lat == "") or (lon == ""):
         return ""
+    else:
+        params = {"lat": lat, "lon": lon, "appid": api_key}
+        url = "http://api.openweathermap.org/data/2.5/weather"
+        Data = requests.get(url, params=params)
+
+        # If it was not a successful api request, returns an empty string
+        if (Data.status_code == 200):
+            usefulData = Data.__dict__
+            weatherDataBytes = usefulData.get('_content')
+            weatherDataDict = json.loads(weatherDataBytes.decode('utf-8'))
+            return weatherDataDict
+        else:
+            return ""
 
 
 # returns the usefule data to display for the api
 def parseData(weatherData):
-    # things i want: temp, feels like, min temp, max temp, wind speed
-    weatherDataBytes = weatherData.get('_content')
-    weatherDataDict = json.loads(weatherDataBytes.decode('utf-8'))
+    if weatherData == "":
+        return ""
+    else:
+        # things i want: temp, feels like, min temp, max temp, wind speed
+        temp = weatherData['main']['temp']
+        feels_like = weatherData['main']['feels_like']
+        temp_min = weatherData['main']['temp_min']
+        temp_max = weatherData['main']['temp_max']
+        humidity = weatherData['main']['humidity']
+        pressure = weatherData['main']['pressure']
+        windSpeed = weatherData['wind']['speed']
+        description = weatherData['weather'][0]['description']
 
-    temp = weatherDataDict['main']['temp']
-    feels_like = weatherDataDict['main']['feels_like']
-    temp_min = weatherDataDict['main']['temp_min']
-    temp_max = weatherDataDict['main']['temp_max']
+        # create the dictionary to return
+        myData = {'description': description, 'temp': temp,
+                "feels_like": feels_like, 'temp_min': temp_min,
+                'temp_max': temp_max, 'humidity': humidity,
+                'pressure': pressure, 'wind_speed': windSpeed}
 
-    # Description
-    return (temp, feels_like, temp_min, temp_max)
-
-
-ident_name = "00AA"
-
-lat, lon = getGeoCoords(ident_name)
-# if lat == "" || lon == "":
-
-
-data = getAirportWeather(lat, lon)
-# if data is "":
-
-# (temp, feels_like, minTemp, maxTemp) = parseData(data)
+        # Description
+        return myData
 
 
 @app.route('/api/current_weather', methods=['GET'])
 def user_api():
-    return jsonify(data)
+
+    ident_name = "KIQ"
+
+    lat, lon = getGeoCoords(ident_name)
+
+    if lat == "" or lon == "":
+        return "<h1>Error: Please enter a valid ident code.</h1>"
+
+    weatherData = getAirportWeather(lat, lon)
+
+    if weatherData == "":
+        return "<h1>Error: API request invalid."
+
+    myData = parseData(weatherData)
+
+    return jsonify(myData)
 
 
 app.run()
